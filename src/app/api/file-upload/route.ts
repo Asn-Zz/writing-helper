09f24ab0c7b0bfc145af67a242ff8122814389d3
Asn-Zz <1771070960@qq.com>
2025-07-21 12:34:43 +0800

    feat: add more file

--- src/app/api/file-upload/route.ts
+++ src/app/api/file-upload/route.ts
@@ -1,18 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import mammoth from 'mammoth';
 
-export const runtime = "edge";
-export const preferredRegion = [
-    "cle1",
-    "iad1",
-    "pdx1",
-    "sfo1",
-    "sin1",
-    "syd1",
-    "hnd1",
-    "kix1",
-];
-
 export async function POST(req: NextRequest) {
     try {
         const formData = await req.formData();
