5550249565730fe5b246f8d75ed70f5b68cdc840
Asn-Zz <1771070960@qq.com>
2025-08-18 15:54:49 +0800

    feat: add image page

--- src/app/features/image-editor/page.tsx
+++ src/app/features/image-editor/page.tsx
@@ -318,7 +318,7 @@ export default function ImageEditor() {
                                     <FaSpinner className="animate-spin mr-2" /> 图像生成中...
                                 </div>
                             ) : contentImages.length > 0 ? (
-                                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
                                     {contentImages.map((image, index) => (
                                         <div className='relative' key={index}>
                                             <img
